15f7740e84d2c63797793428b344e75ccc3e449f
Sergii Bondarenko <broken@firstvector.org>
2018-04-19 12:31:29 +0400

    Fix starting the Docker daemon

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd /usr/local/share/cikit/matrix/roles/api/files/cikit-rest-api
 cikit env/start --ignore-cikit-mount --privileged
 cikit ssh "apt install ssh lxc iptables -y"
 cikit matrix/provision --rest-api
-cikit ssh "service docker start && systemctl enable docker"
+cikit ssh "bash -c 'service docker start && systemctl enable docker'"
 
 if [ "test" == "$1" ]; then
   cikit ssh "bash -c 'cd /var/www/cikit-rest-api && npm run lint && npm test'"
