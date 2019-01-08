570f09ae1a2857e392cea167fa6b19bf7f721981
Sergii Bondarenko <broken@firstvector.org>
2019-01-08 19:49:29 +0400

    Allow tracing logs when "setup.sh" has failed

--- start.sh
+++ start.sh
@@ -6,8 +6,11 @@ cd /usr/local/share/cikit/matrix/roles/api/files/cikit-rest-api
 cikit env/start --ignore-cikit-mount --privileged
 cikit ssh "apt install ssh lxc iptables -y"
 cikit matrix/provision --rest-api
-cikit ssh "bash -c 'service docker start && systemctl enable docker'"
 
-if [ "test" == "$1" ]; then
-  cikit ssh "bash -c 'cd /var/www/cikit-rest-api && npm run lint && npm test'"
+if cikit ssh "bash -c 'service docker start && systemctl enable docker'"; then
+  if [[ "$1" == "test" ]]; then
+    cikit ssh "bash -c 'cd /var/www/cikit-rest-api && npm run lint && npm test'"
+  fi
+else
+  cikit ssh "bash -c 'journalctl --no-pager -xe'"
 fi
