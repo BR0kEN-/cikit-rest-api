e73934b7c55e81e0260078655b1c021022f03dd9
Sergii Bondarenko <broken@firstvector.org>
2019-01-08 21:00:11 +0400

    Do not use overlay for the "containerd"

--- start.sh
+++ start.sh
@@ -7,6 +7,11 @@ cikit env/start --ignore-cikit-mount --privileged
 cikit ssh "apt install ssh lxc iptables linux-headers-\$(uname -r) -y"
 cikit matrix/provision --rest-api
 
+# https://www.linode.com/community/questions/17114/docker-wont-start-using-the-latest-linode-kernel#answer-67349
+cikit ssh "mkdir -p /etc/systemd/system/containerd.service.d/"
+cikit ssh "echo -e '[Service]\nExecStartPre=' > /etc/systemd/system/containerd.service.d/override.conf"
+cikit ssh "systemctl daemon-reload"
+
 if cikit ssh "bash -c 'service docker start && systemctl enable docker'"; then
   if [[ "$1" == "test" ]]; then
     cikit ssh "bash -c 'cd /var/www/cikit-rest-api && npm run lint && npm test'"
